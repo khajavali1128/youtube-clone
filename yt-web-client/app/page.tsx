b33c3c04593ef77ed20bb5b291c28dd65737a552
khajavali1128 <khajavali1128@gmail.com>
2025-02-03 21:55:21 -0500

    add yt-web-client

---
+++ yt-web-client/app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <div className={styles.description}>
+          <p>
+            Get started by editing&nbsp;
+            <code className={styles.code}>app/page.tsx</code>
+          </p>
+        </div>  
+      </main>
+    </div>
+  );
+}
